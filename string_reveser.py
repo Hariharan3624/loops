9c7f7d0cef51497f244c807f963605544fe84502
Hariharan3624 <vikrammanoharan@gmail.com>
2026-01-28 18:11:13 +0000

    Add files via upload

---
+++ string_reveser.py
@@ -0,0 +1,6 @@
+str1 = (input("please type in the string value you want to be reserved: "))
+str2 = ("")
+for i in str1:
+    str2 = i + str1
+print("your string normaly is",str1)
+print("your string reversed is",str2)
